65f9011c01b8088a1cebf35a82c63b27dd746478
b7c8p <morapaezc@gmail.com>
2023-05-05 11:52:07 -0500

    Adding types

---
+++ src/types.d.ts
@@ -0,0 +1,34 @@
+declare global {
+  interface Array<T> {
+    // eslint-disable-next-line @typescript-eslint/method-signature-style
+    toSorted(compareFn?: (a: T, b: T) => number): T[]
+  }
+}
+
+export enum ProgramType {
+  Movie = 'movie',
+  Series = 'series',
+}
+
+export interface Response {
+  total: number
+  entries: Entry[]
+}
+
+export interface Entry {
+  title: string
+  description: string
+  programType: ProgramType
+  images: Images
+  releaseYear: number
+}
+
+export interface Images {
+  'Poster Art': PosterArt
+}
+
+export interface PosterArt {
+  url: string
+  width: number
+  height: number
+}
